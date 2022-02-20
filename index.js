83793ae9bc94ecf3a8c7b9cf07c4e873086ace64
ThomasLe-dev <bathanh2507@gmail.com>
2022-02-20 16:01:08 +0700

    Register and Login auth

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const app = express();
 const PORT = 5000;
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config()
+app.use(express.json());
 
 //connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
 }).then(console.log("connectted to mongodb"))
 .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, ()=>{
     console.log('Running Backend.');
